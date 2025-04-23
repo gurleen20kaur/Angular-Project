d7be9e692885262872dffe5da5428d130c03e093
Gurleen Wahla <gurleen@iciinnovations.com>
2025-04-23 15:35:50 -0230

    new component added

---
+++ angular-project/src/app/task-list/task-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css'
+})
+export class TaskListComponent {
+  newTask: string = '';
+  tasks: string[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+}
